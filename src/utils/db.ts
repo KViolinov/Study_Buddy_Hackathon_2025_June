6934f1fabd7ed7ca5d005b2c783b2a61299ee40b
Atanas <atanaschobanov2008@gmail.com>
2025-06-22 14:18:51 +0300

    Add .env file

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const supabaseUrl = "https://qjbwcxqxtgftawrqxuec.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqYndjeHF4dGdmdGF3cnF4dWVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MTg5NDcsImV4cCI6MjA2NjA5NDk0N30.DWwk1lldHiuZwqocTFMQD3w-cNt8geJaeq1ZeRT84Go";
+const supabaseKey = process.env.SUPABASE_KEY as string;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
