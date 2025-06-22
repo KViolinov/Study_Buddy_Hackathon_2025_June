cf6c5214ba35b327cf9309afea9897eec80ad95b
Mert1026 <mertelsenev10@gmail.com>
2025-06-22 03:16:20 +0300

    Added services

---
+++ StudyBuddy/StudyBuddy/StudyBuddy.Data/Models/InputModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyBuddy.Data.Models
+{
+    public class InputModel
+    {
+        public int userId { get; set; }
+        public string type { get; set; }
+        public string inputSourceType { get; set; }
+        public string inputText { get; set; }
+    }
+}
---
+++ StudyBuddy/StudyBuddy/StudyBuddy.Data/Models/SubModels/QuizModels/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StudyBuddy.Data.Models.SubModels.Quiz
+{
+    public class Quiz
+    {
+        [JsonPropertyName("question")]
+        public string Question { get; set; }
+
+        [JsonIgnore] // Don't serialize
+        public string CorrectAnswer { get; set; }
+
+        [JsonPropertyName("options")]
+        public List<string> Options { get; set; }
+    }
+}
