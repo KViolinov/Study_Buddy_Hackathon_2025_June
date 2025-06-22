32b7b250874a2c12823180265541108cb37b8f52
Atanas <atanaschobanov2008@gmail.com>
2025-06-22 15:33:26 +0300

    Enable CORS for website service

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import inputRoutes from "./routes/inputRoutes";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", inputRoutes);
 
 app.listen(PORT, () => {
